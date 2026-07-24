35011dcf6fb90f31b7738598c4c10f8449410424
Mukesheshwar29 <mukeshkannan.t2024aids@sece.ac.in>
2026-07-24 09:31:21 +0530

    Delete src directory

--- src/store/useSessionStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand'
-
-export const useSessionStore = create((set) => ({
-  sidebarCollapsed: false,
-  toggleSidebar: () => set((state) => ({ sidebarCollapsed: !state.sidebarCollapsed })),
-  setSidebarCollapsed: (collapsed) => set({ sidebarCollapsed: collapsed }),
-  activeChatSessionId: null,
-  setActiveChatSessionId: (id) => set({ activeChatSessionId: id }),
-}))
